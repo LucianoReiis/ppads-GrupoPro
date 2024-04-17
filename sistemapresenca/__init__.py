84df5024563ab084f15cf40c042b99bf9f2c3f5f
LucianoReiis <96396685+LucianoReiis@users.noreply.github.com>
2024-04-16 22:09:59 -0300

    Update __init__.py Variavel Banco de dados online

--- sistemapresenca/__init__.py
+++ sistemapresenca/__init__.py
@@ -2,11 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
+
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'bfddc6059c94aa78f1f3d8be33cbdcf0894e5d8bb8960f6d3d8d3d60eecd43c9'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bancodedados.db'
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL") # Banco de dados Railway (online)
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bancodedados.db' # Banco de dados local
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
