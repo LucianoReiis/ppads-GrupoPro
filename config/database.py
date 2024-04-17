5ce8549f1a2e207f5905f2ee1e80ef07156f210b
LucianoReiis <96396685+LucianoReiis@users.noreply.github.com>
2024-04-16 21:41:20 -0300

    Delete config directory

--- config/database.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb+srv://admin:admin@sistemapresenca.6nhyo7m.mongodb.net/?retryWrites=true&w=majority&appName=SistemaPresenca")
-
-db = client.presence_db
-db2 = client.user_db
-
-collection_presence = db["presence_collection"]
-collection_user = db2["user_collection"]
