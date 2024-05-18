9afb3544e8701e8a39ea61d57ab07df0de5fbd98
LucianoReiis <96396685+LucianoReiis@users.noreply.github.com>
2024-05-18 16:54:28 -0300

    Update __init__.py

--- sistemapresenca/__init__.py
+++ sistemapresenca/__init__.py
@@ -8,10 +8,12 @@ import sqlalchemy
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'bfddc6059c94aa78f1f3d8be33cbdcf0894e5d8bb8960f6d3d8d3d60eecd43c9'
-if os.getenv("DATABASE_URL"):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL") # Banco de dados Railway (online)
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bancodedados.db' # Banco de dados local
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:nmeCPCqQYNonRjsfoFFJVuGlMPLUYLTp@monorail.proxy.rlwy.net:49830/railway'
+
+#if os.getenv("DATABASE_URL"):
+    #app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL") # Banco de dados Railway (online)
+#else:
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bancodedados.db' # Banco de dados local
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -24,7 +26,7 @@ engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 inspector = sqlalchemy.inspect(engine)
 if not inspector.has_table("alunos"):
     with app.app_context():
-        #database.drop_all()
+        database.drop_all()
         database.create_all()
         print("banco de dados criado com sucesso")
 else:
