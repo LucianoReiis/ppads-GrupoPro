d3f54ae342db85f1a36a80325527ca1706c70599
LucianoReiis <96396685+LucianoReiis@users.noreply.github.com>
2024-05-18 17:01:11 -0300

    Update __init__.py

--- sistemapresenca/__init__.py
+++ sistemapresenca/__init__.py
@@ -8,12 +8,10 @@ import sqlalchemy
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'bfddc6059c94aa78f1f3d8be33cbdcf0894e5d8bb8960f6d3d8d3d60eecd43c9'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:nmeCPCqQYNonRjsfoFFJVuGlMPLUYLTp@monorail.proxy.rlwy.net:49830/railway'
-
-#if os.getenv("DATABASE_URL"):
-    #app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL") # Banco de dados Railway (online)
-#else:
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bancodedados.db' # Banco de dados local
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL") # Banco de dados Railway (online)
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bancodedados.db' # Banco de dados local
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
